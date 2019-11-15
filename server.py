e70ae0ebdc6b55b793c2b1a56b89da10fa5cabf8
UrsanRobert <56715674+UrsanRobert@users.noreply.github.com>
2019-11-15 11:32:53 +0200

    Update server.py

--- server.py
+++ server.py
@@ -10,3 +10,19 @@ port=5000
 
 #asocierea la server a adresei(host+port) 
 serversocket.bind( (host,port) )
+
+#crearea unui listener TCP, parametrul fiind numarul de conexiuni alocate
+serversocket.listen(3)
+
+while True:
+    #realizam conexiunea
+    clientsocket,address = serversocket.accept()
+
+    print("Conexiune primita de la " % str(address))
+    
+    #Mesajul trimis clientului in cazul conectarii cu succes
+    message = 'Salut! Iti multumim ca te-ai conectat la serverun nostru' + "\r\n"
+    
+    clientsocket.send(message)
+
+    clientsocket.close()
