165cb54c0ad69d3a316ac7b5d2322dff4a7317cd
UrsanRobert <56715674+UrsanRobert@users.noreply.github.com>
2019-11-15 09:13:10 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,12 @@
 import socket
 # crearea unui socket de tip IPV4, TCP
 serversocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#crearea unui obiect care stocheaza numele host-ului
+host='127.0.0.1'
+
+#definirea unui port unde ne dorim sa cream serverul
+port=5000
+
+#asocierea la server a adresei(host+port) 
+serversocket.bind( (host,port) )
